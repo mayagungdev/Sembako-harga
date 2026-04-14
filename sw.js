24f06896c8451e4f3e869f907368d7cf3276e4bd
Mayagung <agungsaefulloh23@gmail.com>
2026-04-14 22:49:09 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "warung-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/js/db.js",
+  "/js/spreadsheet.js",
+  "/js/kasir.js",
+  "/js/theme.js",
+  "/js/utils.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
